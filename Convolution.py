c15d4d106dc5bcfb42dcef9ffb0ed36a56854b3f
Ian García <iangarciag@outlook.com>
2021-05-06 11:50:50 -0500

    Added dynamic padding
    Based on kernel size, fullconvolution only

--- Convolution.py
+++ Convolution.py
@@ -5,42 +5,10 @@
 import numpy as np
 
 
-def matrix_padding(matrix):
-    """
-    Adds a padding of 0 to a matrix.
-    :param matrix: Target matrix to add padding.
-    :return output: Matrix with padding.
-    """
-    m_row, m_col = matrix.shape
-    output = np.zeros((m_row + 2, m_col + 2))
-
-    for i in range(m_row):
-        for j in range(m_col):
-            output[i + 1, j + 1] = matrix[i, j]
-    return output
-
-
-def matrix_sum(matrix, kernel):
-    """
-    Sums & multiplies a matrix with a kernel.
-    * We assume that matrix and kernel are the same size.
-    * Otherwise an error is expected.
-    :param matrix: Matrix num 1.
-    :param kernel: Matrix num 2.
-    :return output: Sum of multiplication values.
-    """
-    m_row, m_col = matrix.shape
-    output = 0.0
-    for i in range(m_row):
-        for j in range(m_col):
-            output += matrix[i, j] * kernel[i, j]
-    return output
-
-
 def convolution(matrix, kernel):
     """
     Performs convolution of a matrix & a kernel (Both defined by user).
-    * We assume matrix does not have padding.
+    * Padding is added based on kernel size.
     :param matrix: Main matrix to perform convolution.
     :param kernel: Kernel defined by user in order to make the convolution.
     :return output: Convolution result as a matrix.
@@ -48,14 +16,19 @@ def convolution(matrix, kernel):
     m_row, m_col = matrix.shape
     k_row, k_col = kernel.shape
 
-    o_row = (m_row - k_row) + 1
-    o_col = (m_col - k_col) + 1
+    output = np.zeros(matrix.shape)
 
-    output = np.zeros((o_row, o_col))
+    padding_height = int((k_row - 1) / 2)
+    padding_width = int((k_col - 1) / 2)
 
-    for i in range(o_row):
-        for j in range(o_col):
-            output[i, j] = matrix_sum(matrix[i:i + k_row, j:j + k_col], kernel)
+    padded_output = np.zeros((m_row + (2 * padding_height), m_col + (2 * padding_width)))
+
+    padded_output[padding_height:padded_output.shape[0] - padding_height,
+    padding_width:padded_output.shape[1] - padding_width] = matrix
+
+    for i in range(m_row):
+        for j in range(m_col):
+            output[i, j] = np.sum(kernel * padded_output[i:i + k_row, j:j + k_col])
     return output
 
 
@@ -70,4 +43,4 @@ if __name__ == '__main__':
     kernel = np.matrix([[1, 1, 1],
                         [0, 0, 0],
                         [2, 10, 3]])
-    print(convolution(matrix_padding(matrix), kernel))
+    print(convolution(matrix, kernel))
